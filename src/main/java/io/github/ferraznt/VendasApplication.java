5fbe28a2625936579055d0f8ead1d8b7246807cd
Gregório Ferraz <jgregoriof@minsait.com>
2023-02-12 22:41:29 -0300

    Subindo Alterações Referentes a Aula 91. - Concluindo a contrução do App de Vendas e realizando os primeiros empacotamentos. - Projeto finalizado. Indo agora para as aulas complementares.

--- src/main/java/io/github/ferraznt/VendasApplication.java
+++ src/main/java/io/github/ferraznt/VendasApplication.java
@@ -2,11 +2,11 @@ package io.github.ferraznt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-@RestController
-public class VendasApplication {
+//Para que seja possível compilar um WAR é necessário Extender a SpringBootServletInitializer
+public class VendasApplication extends SpringBootServletInitializer {
 
     // Método para salvar um Cliente em Tempo de Execussão.
     public static void main(String[] args) {
